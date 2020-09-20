8d6a5f2e5fe68bf338df91c28876e2d5e1dc8574
Laura Kovačić <laurakciic@gmail.com>
2020-09-20 22:37:19 +0200

    hibernate student

--- src/main/java/hr/laurakciic/zavrsnirad/model/Student.java
+++ src/main/java/hr/laurakciic/zavrsnirad/model/Student.java
@@ -5,10 +5,61 @@
  */
 package hr.laurakciic.zavrsnirad.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Laura
  */
+
+@Entity
+@Table(name = "student")
 public class Student {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String ime;
+    private String prezime;
+    private String jmbag;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getJmbag() {
+        return jmbag;
+    }
+
+    public void setJmbag(String jmbag) {
+        this.jmbag = jmbag;
+    }
+    
+    
     
 }
