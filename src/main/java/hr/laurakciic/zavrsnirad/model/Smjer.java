904fafb9ddce62410482054c88b5cf6436677a58
Laura Kovačić <laurakciic@gmail.com>
2020-09-22 11:37:32 +0200

    kreiranje nadklase Entite te Smjer klase i mapiranje na bazu, promjena edunovajp22_hib u izostanciferit_hib

---
+++ src/main/java/hr/laurakciic/zavrsnirad/model/Smjer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.laurakciic.zavrsnirad.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Laura
+ */
+@Entity
+public class Smjer extends Entitet{
+    
+    private String naziv;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+    
+    
+}
